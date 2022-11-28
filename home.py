92ba1e4899775f030087e7f0dddeb3317499db81
Tristan-Gallo <gallo.tristan@iisgalvanimi.edu.it>
2022-11-28 08:43:35 +0000

    inizio progetto

---
+++ home.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, redirect, url_for
+app = Flask(__name__)
+
+import matplotlib.pyplot as plt
+import matplotlib
+from matplotlib.figure import Figure
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+import folium
+import contextily
+import geopandas as gpd
+import pandas as pd
+import io
+from flask import Flask, render_template, send_file, make_response, url_for, Response, request, redirect
+app = Flask(__name__)
+import re
+
+
+
+
+@app.route('/', methods=['GET'])
+def home():
+
+
+
+
+    return render_template('home.html')
